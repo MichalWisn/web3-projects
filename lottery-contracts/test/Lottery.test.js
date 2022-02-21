63145ccc3bb8062cf3ac3d53c155b61548da66f9
Michal Wisniewski <michal@finixio.com>
2022-02-21 23:23:42 +0100

    feat: Lottery test improvement

--- lottery-contracts/test/Lottery.test.js
+++ lottery-contracts/test/Lottery.test.js
@@ -8,13 +8,16 @@ const web3 = new Web3(ganache.provider());
 describe('Inbox contract', () => {
   let accounts;
   let lottery;
+  let administrator;
+  let player;
 
   beforeEach(async () => {
     accounts = await web3.eth.getAccounts();
+    [administrator, player] = accounts;
 
     lottery = await new web3.eth.Contract(abi)
       .deploy({ data: bytecode })
-      .send({ from: accounts[0], gas: '1000000' });
+      .send({ from: administrator, gas: '1000000' });
   });
 
   it('deploys to test network', () => {
@@ -28,13 +31,13 @@ describe('Inbox contract', () => {
 
   it('allows one account to enter lottery', async () => {
     await lottery.methods.enter().send({
-      from: accounts[0],
+      from: player,
       value: web3.utils.toWei('0.01', 'ether'),
     });
 
     const players = await lottery.methods.getPlayers().call();
     expect(players.length).toBe(1);
-    expect(players[0]).toBe(accounts[0]);
+    expect(players[0]).toBe(player);
   });
 
   it('alows multiple accounts to enter lottery', async () => {
@@ -51,7 +54,7 @@ describe('Inbox contract', () => {
 
   it('throws error if account tries to enter without paying enough', async () => {
     const sendInsufficientMoney = () => lottery.methods.enter().send({
-      from: accounts[0],
+      from: player,
       value: web3.utils.toWei('0.0001', 'ether'),
     });
 
@@ -67,7 +70,7 @@ describe('Inbox contract', () => {
 
   it('allows to select winner only for administrator', async () => {
     const selectWinner = () => lottery.methods.selectWinner().send({
-      from: accounts[2],
+      from: player,
     });
 
     expect(selectWinner()).rejects.toThrow();
@@ -82,7 +85,7 @@ describe('Inbox contract', () => {
 
   it('resets the array when winner is selected', async () => {
     await lottery.methods.enter().send({
-      from: accounts[0],
+      from: player,
       value: web3.utils.toWei('0.01', 'ether'),
     });
 
@@ -90,28 +93,27 @@ describe('Inbox contract', () => {
     expect(players.length).toBe(1);
 
     await lottery.methods.selectWinner().send({
-      from: accounts[0],
+      from: administrator,
     });
 
     players = await lottery.methods.getPlayers().call();
     expect(players.length).toBe(0);
   });
 
-  it('sends money to the winner and resets the array', async () => {
-    const player = accounts[5];
+  it('sends money to the winner', async () => {
     await lottery.methods.enter().send({
       from: player,
       value: web3.utils.toWei('0.01', 'ether'),
     });
 
-    const balanceAfterRegister = await web3.eth.getBalance(player);
+    const balanceAfterEntry = await web3.eth.getBalance(player);
 
     await lottery.methods.selectWinner().send({
-      from: accounts[0],
+      from: administrator,
     });
 
     const finalBalance = await web3.eth.getBalance(player);
 
-    expect(Number(finalBalance)).toBeGreaterThan(Number(balanceAfterRegister));
+    expect(Number(finalBalance)).toBeGreaterThan(Number(balanceAfterEntry));
   });
 });
